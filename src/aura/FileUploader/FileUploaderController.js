ecc79ee02b35a7414c475f74bb346f1491895d88
vineet-puranik <vineet.puranik@docusign.com>
2018-09-13 15:23:05 -0700

    Client DOM injection

--- src/aura/FileUploader/FileUploaderController.js
+++ src/aura/FileUploader/FileUploaderController.js
@@ -8,12 +8,13 @@
 
     //Add message listener
     window.addEventListener("message", $A.getCallback(function (event) {
-      if (event.data.state === 'LOADED') {
+      var state = JSON.parse(JSON.stringify(event.data.state));
+      if (state === 'LOADED') {
         //Set vfHost which will be used later to send message
-        component.set('v.vfHost', event.data.vfHost);
-      } else if (event.data.state === 'uploadFinished') {
-        var messageType = event.data.messageType;
-        var message = event.data.message;
+        component.set('v.vfHost', JSON.parse(JSON.stringify(event.data.vfHost)));
+      } else if (state === 'uploadFinished') {
+        var messageType = JSON.parse(JSON.stringify(event.data.messageType));
+        var message = JSON.parse(JSON.stringify(event.data.message));
 
         if (messageType === 'SUCCESS') {
           component.set('v.message', message);
