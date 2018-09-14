10abebbaf390eb88b25fa3ee319cda8f3206b03a
vineet-puranik <vineet.puranik@docusign.com>
2018-09-13 17:16:56 -0700

    DOM injection changes

--- src/aura/FileUploader/FileUploaderController.js
+++ src/aura/FileUploader/FileUploaderController.js
@@ -3,24 +3,28 @@
     var namespace = component.get('v.namespace');
     var namespaceApi = namespace !== 'c' ? namespace + '__' : '';
     var frameSrc = '/apex/' + namespaceApi + 'fileUploader?id=' + component.get('v.recordId') + '&lcHost=' + component.get('v.lcHost');
-
     component.set('v.frameSrc', frameSrc);
+    helper.setVfHost(component);
 
     //Add message listener
     window.addEventListener("message", $A.getCallback(function (event) {
-      var state = JSON.parse(JSON.stringify(event.data.state));
-      if (state === 'LOADED') {
-        //Set vfHost which will be used later to send message
-        component.set('v.vfHost', JSON.parse(JSON.stringify(event.data.vfHost)));
-      } else if (state === 'uploadFinished') {
-        var messageType = JSON.parse(JSON.stringify(event.data.messageType));
-        var message = JSON.parse(JSON.stringify(event.data.message));
+      var origin = JSON.parse(JSON.stringify(event.data.origin));
+      //check the origin to prevent malicious event capturing
+      if (origin != component.get('v.vfHost')) {
+          return;
+      }
+      //if origin as component attribute match then proceed
+      else {
+        var state = JSON.parse(JSON.stringify(event.data.state));
+        if (state === 'uploadFinished') {
+            var messageType = JSON.parse(JSON.stringify(event.data.messageType));
+            var message = JSON.parse(JSON.stringify(event.data.message));
+                 if (messageType === 'SUCCESS') {
+                  component.set('v.message', message);
 
-        if (messageType === 'SUCCESS') {
-          component.set('v.message', message);
-
-        } else if (messageType === 'ERROR') {
-          component.set('v.message', message);
+                } else if (messageType === 'ERROR') {
+                  component.set('v.message', message);
+                }
         }
       }
     }));
