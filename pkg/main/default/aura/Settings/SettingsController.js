23d0eafce01900f7c4b7fbb729fdb41a02b228ff
Fred Wade <fred.wade@docusign.com>
2019-01-31 09:49:59 -0800

    Fix system sender

--- pkg/main/default/aura/Settings/SettingsController.js
+++ pkg/main/default/aura/Settings/SettingsController.js
@@ -1,19 +1,19 @@
 ({
   initialize: function (component, event, helper) {
     helper.hideToast(component);
-    helper.getSettings(component, helper);
+    helper.getSettings(component);
   },
 
-  onCancel: function(component, event, helper) {
+  onCancel: function (component, event, helper) {
     component.set('v.showExitModal', true);
   },
 
-  onConfirmCancel: function(component, event, helper) {
+  onConfirmCancel: function (component, event, helper) {
     component.set('v.showExitModal', false);
     helper.exit(component);
   },
 
   onSave: function (component, event, helper) {
-    helper.saveSettings(component, helper);
+    helper.saveSettings(component);
   }
 });
