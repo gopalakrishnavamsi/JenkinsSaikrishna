bb3e5d96b0c078d15d89209917fc4ef94b3595f4
vineet-puranik <vineet.puranik@docusign.com>
2019-04-25 14:47:00 -0700

    Agreements Space

--- pkg/main/default/aura/AgreementsExternalReview/AgreementsExternalReviewHelper.js
+++ pkg/main/default/aura/AgreementsExternalReview/AgreementsExternalReviewHelper.js
@@ -76,7 +76,9 @@
 
   hide: function (component) {
     component.find('externalReviewAgreementsModal').hide();
-  }, showToast: function (component, message, mode) {
+  },
+
+  showToast: function (component, message, mode) {
     var evt = component.getEvent('toastEvent');
     evt.setParams({
       show: true, message: message, mode: mode
