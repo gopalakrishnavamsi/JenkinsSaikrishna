bd01482dc3f93644ccdcefe87ad23284384802f5
vineet-puranik <vineet.puranik@docusign.com>
2019-05-07 20:45:05 -0700

    DFS-5247 and DFS-5250 changes

---
+++ pkg/main/default/aura/ExternalReviewRecipient/ExternalReviewRecipientHelper.js
@@ -0,0 +1,16 @@
+({
+  handleRecipientIdChange: function (component) {
+    var r = component.get('v.recipient');
+    var sId = component.get('v.sourceId');
+
+    if (r && sId && (!r.source || (r.source.id !== sId))) {
+      var e = component.getEvent('recipientIdChange');
+      r.source = {
+        id: sId
+      };
+      e.setParams({data: r});
+      e.fire();
+    }
+  }
+
+});
