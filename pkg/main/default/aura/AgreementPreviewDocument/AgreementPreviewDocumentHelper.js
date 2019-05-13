d1e79bf720bbfddf7cf3cfc3559f0f77da72ee3b
Ken Naku <ken.naku@docusign.com>
2019-05-13 12:07:01 -0700

    Fixed styling issues

--- pkg/main/default/aura/AgreementPreviewDocument/AgreementPreviewDocumentHelper.js
+++ pkg/main/default/aura/AgreementPreviewDocument/AgreementPreviewDocumentHelper.js
@@ -1,30 +1,30 @@
 ({
-	loadWidget: function(component, id, documentUrl, documentName, historyItems) {
-		try {
-			console.log('payload: ', typeof documentUrl);
-			var widet = new SpringCM.Widgets.Preview({
-			    iconPath: $A.get('$Resource.scmwidgetsspritemap'),
-			    accessTokenFn: this.getAccessToken.bind(component),
-			    uploadApiBaseDomain: "https://apiuploadna11.springcm.com",
-			    downloadApiBaseDomain: "https://apidownloadna11.springcm.com"
-			});
-			widet.render('#agreementDocumentView');
-			widet.renderDocumentPreview({
-		        name: documentName,
-		        href: documentUrl,
-		        hasPdfPreview: true,
-		        uid: id
-	    	});
-	    	widet.history.setHistoryItems(Object.assign([],historyItems));
-		} catch(err) {
-			console.log('error loading preview', err);
-		}
-	},
+    loadWidget: function(component, id, documentUrl, documentName, historyItems) {
+        try {
+            console.log('payload: ', typeof documentUrl);
+            var widet = new SpringCM.Widgets.Preview({
+                iconPath: $A.get('$Resource.scmwidgetsspritemap'),
+                accessTokenFn: this.getAccessToken.bind(component),
+                uploadApiBaseDomain: "https://apiuploadna11.springcm.com",
+                downloadApiBaseDomain: "https://apidownloadna11.springcm.com"
+            });
+            widet.render('#agreementDocumentView');
+            widet.renderDocumentPreview({
+                name: documentName,
+                href: documentUrl,
+                hasPdfPreview: true,
+                uid: id
+            });
+            widet.history.setHistoryItems(Object.assign([], historyItems));
+        } catch (err) {
+            console.log('error loading preview', err);
+        }
+    },
 
-	//FixMe: Refrence Server side action for getting AccessToken and resolve the results. 
-	getAccessToken: function(component) {
-		return new Promise(function(resolve){
-			resolve('12345667');
-		});
-	}
+    //FixMe: Refrence Server side action for getting AccessToken and resolve the results. 
+    getAccessToken: function(component) {
+        return new Promise(function(resolve) {
+            resolve('12345667');
+        });
+    }
 })
