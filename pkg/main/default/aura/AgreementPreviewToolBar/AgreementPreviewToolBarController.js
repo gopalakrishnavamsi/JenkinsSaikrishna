0d1ba65f5b91304436b666483ac8d49248bcd0eb
Ken Naku <ken.naku@docusign.com>
2019-05-13 15:08:02 -0700

    Fixed styling issues, unit tests for NegotiateService and AgreementsController coming shortly..

--- pkg/main/default/aura/AgreementPreviewToolBar/AgreementPreviewToolBarController.js
+++ pkg/main/default/aura/AgreementPreviewToolBar/AgreementPreviewToolBarController.js
@@ -1,40 +1,40 @@
 ({
 
-  rename: function(component, event, helper) {
-  	var actions = component.get('v.AgreementActionManager');
-  	var agreement = component.get('v.Agreement');
-  	actions.rename(agreement, component);
+    rename: function(component, event, helper) {
+        var actions = component.get('v.AgreementActionManager');
+        var agreement = component.get('v.Agreement');
+        actions.rename(agreement, component);
 
-  },
+    },
 
-  delete: function(component, event, helper) {
-  	var actions = component.get('v.AgreementActionManager');
-  	var agreement = component.get('v.Agreement');
-  	actions.delete(agreement, component);
-  },
+    delete: function(component, event, helper) {
+        var actions = component.get('v.AgreementActionManager');
+        var agreement = component.get('v.Agreement');
+        actions.delete(agreement, component);
+    },
 
-  internalReview: function(component, event, helper) {
-  	var actions = component.get('v.AgreementActionManager');
-  	var agreement = component.get('v.Agreement');
-  	actions.internalReview(agreement, component);
-  },
+    internalReview: function(component, event, helper) {
+        var actions = component.get('v.AgreementActionManager');
+        var agreement = component.get('v.Agreement');
+        actions.internalReview(agreement, component);
+    },
 
-  externalReview: function(component, event, helper) {
-  	var actions = component.get('v.AgreementActionManager');
-  	var agreement = component.get('v.Agreement');
-  	actions.externalReview(agreement, component);
-  },
+    externalReview: function(component, event, helper) {
+        var actions = component.get('v.AgreementActionManager');
+        var agreement = component.get('v.Agreement');
+        actions.externalReview(agreement, component);
+    },
 
-  upload: function(component, event, helper) {
-  	var actions = component.get('v.AgreementActionManager');
-  	var agreement = component.get('v.Agreement');
-  	actions.upload(component);
-  },
+    upload: function(component, event, helper) {
+        var actions = component.get('v.AgreementActionManager');
+        var agreement = component.get('v.Agreement');
+        actions.upload(component);
+    },
 
-  share: function(component, event, helper) {
-  	var actions = component.get('v.AgreementActionManager');
-  	var agreement = component.get('v.Agreement');
-  	actions.share(agreement, component);
-  }
-  
+    share: function(component, event, helper) {
+        var actions = component.get('v.AgreementActionManager');
+        var agreement = component.get('v.Agreement');
+        actions.share(agreement, component);
+    }
+
 });
