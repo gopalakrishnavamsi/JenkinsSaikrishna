7c8c2f2123826c99624a225e50acf637f2b15efd
vineet-puranik <vineet.puranik@docusign.com>
2019-05-15 20:03:58 -0700

    Formatting changes

--- pkg/main/default/aura/AgreementsImport/AgreementsImportController.js
+++ pkg/main/default/aura/AgreementsImport/AgreementsImportController.js
@@ -1,55 +1,51 @@
 ({
-  onInit: function(component) {
-    component.set("v.currentStep", "1");
-    component.set("v.disableSalesforceFileImport", true);
+  onInit: function (component, event, helper) {
+    helper.initialize(component);
   },
 
-  navigateToFileSelection: function(component, event, helper) {
+  navigateToFileSelection: function (component, event, helper) {
     helper.getSalesforceFiles(component, event, helper);
   },
 
-  navigateToUploadFilesfromPC: function(component, event, helper) {
-    component.set("v.currentStep", "3");
-    helper.uploadFile(component);
+  navigateToUploadFilesfromPC: function (component, event, helper) {
+    helper.uploadFilesFromPC(component, event, helper);
   },
 
-  backButtonClicked: function(component) {
-    component.set("v.currentStep", "1");
+  backButtonClicked: function (component) {
+    component.set('v.currentStep', '1');
   },
 
-  importButtonClicked: function(component, event, helper) {
+  importButtonClicked: function (component, event, helper) {
     helper.publishAgreement(component, event, helper);
   },
 
-  handleFileSelection: function(component, event, helper) {
+  handleFileSelection: function (component, event, helper) {
     //checkbox checked
-    if (event.getSource().get("v.checked")) {
-      var selectedValue = event.getSource().get("v.value");
+    if (event.getSource().get('v.checked')) {
+      var selectedValue = event.getSource().get('v.value');
       helper.setSelectedFiles(component, selectedValue);
     }
     //checkbox unchecked
     else {
-      component.set("v.disableSalesforceFileImport", true);
+      component.set('v.disableSalesforceFileImport', true);
     }
   },
 
-  uploadFileButtonClicked: function(component) {
-    component.set("v.currentStep", "1");
+  uploadFileButtonClicked: function (component) {
+    component.set('v.currentStep', '1');
   },
 
-  uploadFileImportButtonClicked: function(component, event, helper) {
+  uploadFileImportButtonClicked: function (component, event, helper) {
     helper.uploadContent(component, event, helper);
   },
 
-  uploadScriptsLoaded: function() {
-    // FIXME: Implement or remove.
-  },
 
-  cancelButtonClicked: function(component, event, helper) {
+  cancelButtonClicked: function (component, event, helper) {
     helper.close(component);
   },
 
-  clickDone: function(component, event, helper) {
+  clickDone: function (component, event, helper) {
     helper.completeImport(component, event, helper);
   }
+
 });
