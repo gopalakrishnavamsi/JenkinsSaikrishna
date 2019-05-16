a69e8a18623fe167d414f9f43a4a393e3640698d
vineet-puranik <vineet.puranik@docusign.com>
2019-05-15 19:30:13 -0700

    Toast and reload of the Preview page after user takes an action

--- pkg/main/default/aura/AgreementPreview/AgreementPreviewController.js
+++ pkg/main/default/aura/AgreementPreview/AgreementPreviewController.js
@@ -1,29 +1,14 @@
 ({
-  onLoad: function (component) {
-    var agreementId = component.get("v.agreementId");
-    var action = component.get('c.getNameSpace');
-    var uiHelper = new UIHelper(function () {
-      return component.getEvent('loadingEvent');
-    }, function () {
-      return component.getEvent('toastEvent');
-    });
-    component.set('v.uiHelper', uiHelper);    
-    action.setCallback(this, function (response) {
-      var state = response.getState();
-      if (state === "SUCCESS") {
-        var manager = new AgreementActionManager('modalContent', response.getReturnValue());
-        manager.getAgreement(component, agreementId)
-        .then(function(agreement) {
-          component.set('v.agreement', agreement);
-          component.set('v.agreementActionManager', manager);
-          component.set('v.loading', false); 
-        }).catch(function(error) {
-          uiHelper.showToast(uiHelper.ToastMode.ERROR, error);
-        });   
-      }
-      else if (state === "ERROR") uiHelper.showToast(uiHelper.ToastMode.ERROR, uiHelper.getErrorMessage(response));
-    });
-    $A.enqueueAction(action);  
+  onLoad: function (component, event, helper) {
+    helper.getAgreementDetails(component);
+  },
+
+  handleToastEvent: function (component, event, helper) {
+    helper.toastEvent(component, event, helper);
+  },
+
+  handleLoadingEvent: function (component, event, helper) {
+    helper.loadingEvent(component, event, helper);
   }
 
 });
