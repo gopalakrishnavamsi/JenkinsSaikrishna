b1edacba9e304e529ef662dfd086880be41b7381
Pratik Hatkar <pratik.hatkar@dsa015096.docusignhq.com>
2019-05-23 15:00:16 -0700

    esLint changes

--- pkg/main/default/aura/AgreementDetails/AgreementDetailsController.js
+++ pkg/main/default/aura/AgreementDetails/AgreementDetailsController.js
@@ -1,37 +1,37 @@
 ({
-  viewAgreement: function(component, event, helper) {
+  viewAgreement: function (component, event, helper) {
     helper.launchAgreementPreview(component);
   },
 
-  showTimeLine: function(component, event, helper) {
+  showTimeLine: function (component, event, helper) {
     helper.showHistoryTimeLine(component);
   },
 
-  showInternalApprovalModal: function(component, event, helper) {
+  showInternalApprovalModal: function (component, event, helper) {
     helper.createInternalApprovalComponent(component, event, helper);
   },
 
-  showExternalReviewModal: function(component, event, helper) {
+  showExternalReviewModal: function (component, event, helper) {
     helper.createExternalReviewComponent(component, event, helper);
   },
 
-  showDeleteModal: function(component, event, helper) {
+  showDeleteModal: function (component, event, helper) {
     helper.createDeleteComponent(component, event, helper);
   },
 
-  showUploadModal: function(component, event, helper) {
+  showUploadModal: function (component, event, helper) {
     helper.createUploadComponent(component, event, helper);
   },
 
-  showDownloadModal: function(component, event, helper) {
+  showDownloadModal: function (component, event, helper) {
     helper.createDownloadComponent(component, event, helper);
   },
 
-  showRenameModal: function(component, event, helper) {
+  showRenameModal: function (component, event, helper) {
     helper.createRenameComponent(component, event, helper);
   },
 
-  showShareLinkModal: function(component, event, helper) {
+  showShareLinkModal: function (component, event, helper) {
     helper.createShareLinkComponent(component, event, helper);
   }
 });
