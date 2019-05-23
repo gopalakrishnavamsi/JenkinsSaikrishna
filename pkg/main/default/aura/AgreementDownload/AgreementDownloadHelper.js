4d1f5f63927abcd536335f8fa1be08129c0adbd2
Pratik Hatkar <pratik.hatkar@dsa015096.docusignhq.com>
2019-05-23 16:22:10 -0700

    adding stringUtils and apiBaseUrl

--- pkg/main/default/aura/AgreementDownload/AgreementDownloadHelper.js
+++ pkg/main/default/aura/AgreementDownload/AgreementDownloadHelper.js
@@ -1,33 +1,31 @@
 ({
-  downloadAgreement: function(component, event, helper) {
+  downloadAgreement: function (component, event, helper) {
     var agreement = component.get("v.agreementDetails");
     var limitedAccessToken = component.get("c.generateDownloadToken");
     limitedAccessToken.setParams({
       objectId: agreement.id.value
     });
-    limitedAccessToken.setCallback(this, function(response) {
+    limitedAccessToken.setCallback(this, function (response) {
       var state = response.getState();
       var result = response.getReturnValue();
       if (state === "SUCCESS") {
         try {
           var downloadWidget = SpringCM.Widgets.Download.downloadDocument(
-            result.apiBaseUrl,
+            result.apiDownloadBaseUrl,
             result.token,
             result.accountId.value,
             agreement.id.value,
             agreement.name
           );
           downloadWidget
-            .then(function() {
+            .then(function () {
               helper.showToast(
                 component,
-                $A.get("$Label.c.SuccessDownloadingFile") +
-                  " " +
-                  agreement.name,
+                stringUtils.format("{0} {1}", $A.get("$Label.c.SuccessDownloadingFile"), agreement.name),
                 "success"
               );
             })
-            .catch(function() {
+            .catch(function () {
               helper.showToast(
                 component,
                 $A.get("$Label.c.ErrorDownloadingFile"),
@@ -52,7 +50,7 @@
     });
     $A.enqueueAction(limitedAccessToken);
   },
-  showToast: function(component, message, mode) {
+  showToast: function (component, message, mode) {
     var fireToastEvent = component.getEvent("toastEvent");
     fireToastEvent.setParams({
       show: true,
