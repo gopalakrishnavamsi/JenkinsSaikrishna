4bb89d772bd4c7f18071f69fe178bc00b32c1dbf
vineet-puranik <vineet.puranik@docusign.com>
2019-06-04 16:03:26 -0700

    approval flow, user invite updates, external reviewer resolution

--- pkg/main/default/aura/AgreementsInternalApproval/AgreementsInternalApprovalController.js
+++ pkg/main/default/aura/AgreementsInternalApproval/AgreementsInternalApprovalController.js
@@ -1,12 +1,6 @@
 ({
   onInit: function(component, event, helper) {
-    //initialize recipients
-    var recipients = component.get('v.recipients');
-    recipients.push(helper.newRecipient());
-    component.set('v.recipients', recipients);
-
-    //set the current step to 1
-    component.set('v.currentStep', '1');
+    helper.onInit(component, event, helper);
   },
 
   handleRecipientChange: function(component, event, helper) {
@@ -14,100 +8,35 @@
   },
 
   addRecipient: function(component, event, helper) {
-    var recipients = component.get('v.recipients');
-    recipients.push(helper.newRecipient());
-    component.set('v.recipients', recipients);
+    helper.addRecipient(component, event, helper);
   },
 
-  removeRecipient: function(component, event) {
-    var recipients = component.get('v.recipients');
-    recipients.splice(event.getSource().get('v.value'), 1);
-    component.set('v.recipients', recipients);
+  removeRecipient: function (component, event, helper) {
+    helper.removeRecipient(component, event);
   },
 
-  onApproverDrag: function(component, event) {
-    event.dataTransfer.setData('Text', '');
-    if (
-      event.currentTarget.id &&
-      !$A.util.isUndefinedOrNull(parseInt(event.currentTarget.id))
-    ) {
-      component.set('v.draggedId', parseInt(event.currentTarget.id));
-    }
+  onApproverDrag: function (component, event, helper) {
+    helper.onApproverDrag(component, event);
   },
 
   allowDrop: function(component, event) {
     event.preventDefault();
   },
 
-  onApproverDrop: function(component, event) {
-    if (
-      event.currentTarget.id &&
-      !$A.util.isUndefinedOrNull(parseInt(event.currentTarget.id))
-    ) {
-      component.set('v.droppedId', parseInt(event.currentTarget.id));
-    }
-
-    var draggedId = component.get('v.draggedId');
-    var droppedId = component.get('v.droppedId');
-    var recipients = component.get('v.recipients');
-
-    if (
-      draggedId !== 'undefined' &&
-      droppedId !== 'undefined' &&
-      recipients !== 'undefined'
-    ) {
-      if (
-        recipients[droppedId] !== 'undefined' &&
-        recipients[draggedId] !== 'undefined' &&
-        recipients[draggedId].name !== 'undefined' &&
-        recipients[draggedId].name !== '' &&
-        recipients[droppedId].name !== 'undefined' &&
-        recipients[droppedId].name !== ''
-      ) {
-        var temp = recipients[draggedId];
-        recipients.splice(draggedId, 1);
-        recipients.splice(droppedId, 0, temp);
-        component.set('v.recipients', recipients);
-      }
-    }
+  onApproverDrop: function (component, event, helper) {
+    helper.onApproverDrop(component, event);
   },
 
-  setApprovalOrder: function(component) {
-    //set the attribute showApprovalOrder based on Checkbox value
-    var isChecked = component.find('approvalOrderCheckbox').get('v.checked');
-    component.set('v.showApprovalOrder', isChecked);
+  setApprovalOrder: function (component, event, helper) {
+    helper.setApprovalOrder(component);
   },
 
   backButtonClicked: function(component, event, helper) {
-    var currentStep = component.get('v.currentStep');
-    //currentStep is Select Recipients
-    if (currentStep === '1') {
-      helper.hide(component, event, helper);
-    }
-    //currentStep is Edit your Message then direct user back to Select Recipients screen
-    if (currentStep === '2') {
-      component.set('v.currentStep', '1');
-    }
+    helper.backButtonClicked(component, event, helper);
   },
 
   nextButtonClicked: function(component, event, helper) {
-    var currentStep = component.get('v.currentStep');
-    if (currentStep === '1') {
-      //Proceed to the personalize message step
-      component.set('v.currentStep', '2');
-    }
-    if (currentStep === '2') {
-      //If successful hide the component
-      helper.hide(component, event, helper);
-      //set the current step to 1
-      component.set('v.currentStep', '1');
-      //display toast notification
-      helper.showToast(
-        component,
-        'Your document has been sent for approval.',
-        'success'
-      );
-    }
+    helper.nextButtonClicked(component, event, helper);
   },
 
   handlePillCloseEvent: function(/*component, event, helper*/) {
