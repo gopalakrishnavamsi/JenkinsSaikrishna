47ad4f990d93adf6c93e5f34d4885967a32bc2b0
Ramprasad Bommaganty <Ramprasad.Bommaganty@docusign.com>
2019-08-16 20:56:38 -0700

    Removed components that are not reqd

--- pkg/main/default/aura/CLMHelpFAQs/CLMHelpFAQsController.js
+++
@@ -1,9 +0,0 @@
-({
-    toggleSection : function(component, event, helper) {
-        var acc = component.find('objMapping');
-        for(var cmp in acc) {
-            $A.util.toggleClass(acc[cmp], 'slds-show');  
-            $A.util.toggleClass(acc[cmp], 'slds-hide');  
-        }
-    },
-})
