b3a6c4549b723579c3b0a03ba1bd63acd8ea9ca7
Vineet.Puranik <vineet.puranik@docusign.com>
2019-08-25 10:59:56 -0700

    updating buttons on SetupWizard

--- pkg/main/default/aura/SetupWizard/SetupWizardController.js
+++ pkg/main/default/aura/SetupWizard/SetupWizardController.js
@@ -8,6 +8,30 @@
     component.set('v.selectedItem', selected);
   },
 
+  navigateToEsignatureTab: function (component) {
+    component.set('v.selectedItem', 'tab_eSignature');
+  },
+
+  navigateToDocumentGenerationTab: function (component) {
+    component.set('v.selectedItem', 'tab_documentGeneration');
+  },
+
+  navigateToNegotiationTab: function (component) {
+    component.set('v.selectedItem', 'tab_negotiation');
+  },
+
+  navigateToUserManagementTab: function (component) {
+    component.set('v.selectedItem', 'tab_userManagement');
+  },
+
+  navigateToComponentsTab: function (component) {
+    component.set('v.selectedItem', 'tab_components');
+  },
+
+  navigateToHelpTab: function (component) {
+    component.set('v.selectedItem', 'tab_help');
+  },
+
   triggerLogout: function (component) {
     component.destroy();
   }
