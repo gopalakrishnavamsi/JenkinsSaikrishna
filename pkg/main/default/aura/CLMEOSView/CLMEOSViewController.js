1b6637e674da129e88cda829f5f13163da01a513
Ramprasad Bommaganty <Ramprasad.Bommaganty@docusign.com>
2019-09-04 09:49:34 -0700

    Initial commit-pushed UI components

---
+++ pkg/main/default/aura/CLMEOSView/CLMEOSViewController.js
@@ -0,0 +1,21 @@
+({
+	onInit: function (component, event, helper) {
+		var action = component.get('c.getEOSUrl');
+		action.setParams(
+			{
+				recordId: component.get('v.recordId')
+			});
+		action.setCallback(this, function (response) {
+			var state = response.getState();
+			if (state === 'SUCCESS') {
+				component.set('v.eosDetails', response.getReturnValue());
+			} else {
+				var errors = response.getError();
+				if (errors && errors[0] && errors[0].message) {
+					helper.fireToast(component, errors[0].message, helper.ERROR);
+				}
+			}
+		});
+		$A.enqueueAction(action);
+	}
+});
