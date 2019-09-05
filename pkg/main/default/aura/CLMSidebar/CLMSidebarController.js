81873e7ee767b1ad47c3f6fe15edc89299744b12
Ramprasad Bommaganty <Ramprasad.Bommaganty@docusign.com>
2019-09-05 10:35:20 -0700

    Bugfix:DFS-5925/96

--- pkg/main/default/aura/CLMSidebar/CLMSidebarController.js
+++ pkg/main/default/aura/CLMSidebar/CLMSidebarController.js
@@ -1,21 +1,21 @@
 ({
-  fireComponentEvent: function (component, event, helper) {
-    var index = event.currentTarget.id;
-    component.set('v.currentIndex', index);
-    helper.fireApplicationEvent(component, {
-      fromComponent: 'CLMSidebar',
-      toComponent: 'CLMSetupLayout',
-      type: 'update',
-      tabIndex: index,
-    }, 'CLMNavigationEvent');
-  },
+	fireComponentEvent: function (component, event, helper) {
+		var index = event.currentTarget.id;
+		component.set('v.currentIndex', index);
+		helper.fireApplicationEvent(component, {
+			fromComponent: 'CLMSidebar',
+			toComponent: 'CLMSetupLayout',
+			type: 'update',
+			tabIndex: index,
+		}, 'CLMNavigationEvent');
+	},
 
-  updateState: function (component, event) {
-    var tabIndex = event.getParam('tabIndex');
-    var fromComponent = event.getParam('fromComponent');
-    var toComponent = event.getParam('toComponent');
-    if (toComponent === 'CLMSetupLayout' && fromComponent !== 'CLMSetupLayout') {
-      component.set('v.currentIndex', tabIndex);
-    }
-  }
+	updateState: function (component, event) {
+		var tabIndex = event.getParam('tabIndex');
+		var fromComponent = event.getParam('fromComponent');
+		var toComponent = event.getParam('toComponent');
+		if (toComponent === 'CLMSetupLayout' && fromComponent !== 'CLMSetupLayout') {
+			component.set('v.currentIndex', tabIndex);
+		}
+	}
 });
