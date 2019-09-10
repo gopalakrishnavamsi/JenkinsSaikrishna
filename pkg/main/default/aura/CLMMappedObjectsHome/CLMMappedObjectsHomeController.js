5fe50872a2a7c7c42987bf6e7755f623078c9d76
Ramprasad Bommaganty <Ramprasad.Bommaganty@docusign.com>
2019-09-10 12:35:35 -0700

    Initial commit - 5949,5945

--- pkg/main/default/aura/CLMMappedObjectsHome/CLMMappedObjectsHomeController.js
+++ pkg/main/default/aura/CLMMappedObjectsHome/CLMMappedObjectsHomeController.js
@@ -1,135 +1,176 @@
 ({
-  onInit: function (component, event, helper) {
-    helper.callServer(component, 'c.getMappedObjectsList', false, function (result) {
-      var data = result;
-      if (data.Account && data.Opportunity && Object.values(data).length === 2) {
-        helper.fireApplicationEvent(component, {
-          fromComponent: 'CLMMappedObjectsHome',
-          toComponent: 'CLMScopedNotifications',
-          type: 'show'
-        }, 'CLMEvent');
-      }
-      component.set('v.mappedObjData', Object.values(result));
-    });
-    helper.createComponent(component, 'c:CLMModelFooterButton', {
-      primaryButtonLabel: $A.get('$Label.c.Remove'),
-      secondaryButtonLabel: $A.get('$Label.c.Cancel'),
-      primaryButtonVariant: 'destructive'
-    }, function (newCmp) {
-      component.set('v.strikeModelFooterButtons', newCmp);
-    });
-  },
+	onInit: function (component, event, helper) {
+		var namespace;
+		helper.callServer(component, 'c.getNamespace', false, function (result) {
+			namespace = result;
+		});
+		var column = [
+			{
+				label: $A.get('$Label.c.SalesforceObject'),
+				fieldName: 'Name',
+				type: 'text',
+				sortable: true,
+				cellAttributes: { alignment: 'left' }
+			},
+			{
+				label: $A.get('$Label.c.ObjectFolderName'),
+				fieldName: (namespace === 'c') ? 'FolderName__c' : 'dfsle__FolderName__c',
+				type: 'text',
+				sortable: true,
+				cellAttributes: { alignment: 'left' }
+			},
+			{
+				label: $A.get('$Label.c.PathInDocuSignCLM'),
+				fieldName: (namespace === 'c') ? 'Path__c' : 'dfsle__Path__c',
+				type: 'text',
+				sortable: true,
+				cellAttributes: { alignment: 'left' }
+			},
+			{
+				type: 'button',
+				initialWidth: 80,
+				typeAttributes: {
+					label: $A.get('$Label.c.Edit'),
+					name: $A.get('$Label.c.Edit'),
+					title: $A.get('$Label.c.Edit'),
+					disabled: false,
+					value: 'edit'
+				}
+			},
+			{
+				type: 'button',
+				initialWidth: 100,
+				typeAttributes: {
+					label: $A.get('$Label.c.Remove'),
+					name: $A.get('$Label.c.Remove'),
+					title: $A.get('$Label.c.Remove'),
+					disabled: false,
+					value: 'remove'
+				}
+			}
+		];
+		component.set('v.mapColumns', column);
+		helper.callServer(component, 'c.getMappedObjectsList', false, function (result) {
+			var data = result;
+			if (data.Account && data.Opportunity && Object.values(data).length === 2) {
+				helper.fireApplicationEvent(component, {
+					fromComponent: 'CLMMappedObjectsHome',
+					toComponent: 'CLMScopedNotifications',
+					type: 'show'
+				}, 'CLMEvent');
+			}
+			component.set('v.mappedObjData', Object.values(result));
+		});
+		helper.createComponent(component, 'c:CLMModelFooterButton', {
+			primaryButtonLabel: $A.get('$Label.c.Remove'),
+			secondaryButtonLabel: $A.get('$Label.c.Cancel'),
+			primaryButtonVariant: 'destructive'
+		}, function (newCmp) {
+			component.set('v.strikeModelFooterButtons', newCmp);
+		});
+	},
 
-  gotoNew: function (component, event, helper) {
-    //fire event to update breadcrumb
-    helper.fireApplicationEvent(component, {
-      navigateTo: { index: '2' },
-      fromComponent: 'CLMMappedObjectsHome',
-      toComponent: 'CLMBreadcrumbs'
-    }, 'CLMBreadcrumbsEvent');
-    //fire event to display CLMCardModel
-    helper.fireApplicationEvent(component, {
-      componentName: 'CLMCardModel',
-      fromComponent: 'CLMMappedObjectsHome',
-      toComponent: 'CLMIntegrationLayout',
-      type: 'show'
-    }, 'CLMNavigationEvent');
-  },
+	gotoNew: function (component, event, helper) {
+		//fire event to update breadcrumb
+		helper.fireApplicationEvent(component, {
+			navigateTo: { index: '2' },
+			fromComponent: 'CLMMappedObjectsHome',
+			toComponent: 'CLMBreadcrumbs'
+		}, 'CLMBreadcrumbsEvent');
+		//fire event to display CLMCardModel
+		helper.fireApplicationEvent(component, {
+			componentName: 'CLMCardModel',
+			fromComponent: 'CLMMappedObjectsHome',
+			toComponent: 'CLMIntegrationLayout',
+			type: 'show'
+		}, 'CLMNavigationEvent');
+	},
 
-  edit: function (component, event) {
-    var id = event.getSource().get('v.value');
-    component.set('v.tempMappingModelDataHolder', {
-      id: id,
-      type: 'edit'
-    });
-    var modelTitleText = $A.get('$Label.c.ConfirmEdits');
-    var modelbodyText = $A.get('$Label.c.EditModalBody');
-    component.set('v.modelTitleText', modelTitleText);
-    component.set('v.modelbodyText', modelbodyText);
-    component.set('v.modelPrimaryButtonText', 'Confirm');
-    component.set('v.modelSecondaryButtonText', 'Cancel');
-    component.set('v.showModal', 'true');
-    var modelComponent = component.find('popupModel');
-    setTimeout($A.getCallback(function () {
-      modelComponent.show();
-    }), 5);
-    var cmpEvent = component.getEvent('CLMScopedNotificationEvent');
-    cmpEvent.fire();
-  },
+	removeMappingModalHandler: function (component, event, helper) {
+		var data = component.get('v.tempMappingModelDataHolder');
+		var modalComponent = component.find('popupModal');
+		var mappedObjData = component.get('v.mappedObjData');
+		var objDetails;
+		mappedObjData.forEach(function (obj) {
+			if (obj.Id === data.id) {
+				objDetails = obj;
+			}
+		});
+		if (data.type === 'remove') {
+			helper.callServer(
+				component,
+				'c.removeMappedObject',
+				{
+					name: objDetails.Name
+				},
+				function () {
+					helper.fireApplicationEvent(
+						component,
+						{
+							fromComponent: 'CLMMappedObjectsHome',
+							toComponent: 'CLMScopedNotifications',
+							type: 'hide'
+						},
+						'CLMEvent'
+					);
+					helper.fireToast(
+						component,
+						stringUtils.format(
+							$A.get('$Label.c.ObjectRemoved'),
+							objDetails.Name
+						),
+						helper.SUCCESS
+					);
+					component.getEvent('CLMScopedNotificationEvent').fire();
+					component.set('v.isRemove', 'false');
+					var newObjectList = [];
+					mappedObjData.forEach(function (obj) {
+						if (obj.Id !== data.id) {
+							newObjectList.push(obj);
+						}
+					});
+					component.set('v.mappedObjData', newObjectList);
+					modalComponent.hide();
+				}
+			);
+		}
+		component.set('v.showModal', 'false');
+	},
 
-  remove: function (component, event, helper) {
-    var id = event.getSource().get('v.value');
-    component.set('v.tempMappingModelDataHolder', {
-      id: id,
-      type: 'remove'
-    });
-    helper.createComponent(component, 'c:CLMModelFooterButton', {
-      primaryButtonLabel: $A.get('$Label.c.Remove'),
-      secondaryButtonLabel: $A.get('$Label.c.Cancel'),
-      primaryButtonVariant: 'destructive'
-    }, function (newCmp) {
-      component.set('v.strikeModelFooterButtons', newCmp);
-      component.set('v.isRemove', 'true');
-    });
-    var modelTitle = $A.get('$Label.c.RemoveMapping');
-    var modelbody = $A.get('$Label.c.RemoveModalBody');
-    component.set('v.modelTitleText', modelTitle);
-    component.set('v.modelbodyText', modelbody);
-    component.set('v.modelPrimaryButtonText', 'Remove');
-    component.set('v.modelSecondaryButtonText', 'Cancel');
-    component.set('v.showModal', 'true');
-    var modelComponent = component.find('popupModel');
-    setTimeout($A.getCallback(function () {
-      modelComponent.show();
-    }), 5);
-  },
+	closeModal: function (component) {
+		component.set('v.showModal', 'false');
+		component.set('v.isRemove', 'false');
+	},
 
-  editMappingModelHandler: function (component, event, helper) {
-    var data = component.get('v.tempMappingModelDataHolder');
-    var modelComponent = component.find('popupModel');
-    var mappedObjData = component.get('v.mappedObjData');
-    var objDetails;
-    mappedObjData.forEach(function (obj) {
-      if (obj.Id === data.id) {
-        objDetails = obj;
-      }
-    });
-    if (data.type === 'remove') {
-      helper.callServer(component, 'c.removeMappedObject', {
-        name: objDetails.Name
-      }, function () {
-        helper.fireApplicationEvent(component, {
-          fromComponent: 'CLMMappedObjectsHome',
-          toComponent: 'CLMScopedNotifications',
-          type: 'hide'
-        }, 'CLMEvent');
-        helper.fireToast(component, stringUtils.format($A.get('$Label.c.ObjectRemoved'), objDetails.Name), helper.SUCCESS);
-        component.getEvent('CLMScopedNotificationEvent').fire();
-        component.set('v.isRemove', 'false');
-        var newObjectList = [];
-        mappedObjData.forEach(function (obj) {
-          if (obj.Id !== data.id) {
-            newObjectList.push(obj);
-          }
-        });
-        component.set('v.mappedObjData', newObjectList);
-        modelComponent.hide();
-      });
-    }
-    else if (data.type === 'edit') {
-      helper.fireApplicationEvent(component, {
-        data: { objDetails: objDetails },
-        componentName: 'CLMCardModel',
-        fromComponent: 'CLMMappedObjectsHome',
-        toComponent: 'CLMIntegrationLayout',
-        type: 'edit'
-      }, 'CLMNavigationEvent');
-    }
-    component.set('v.showModal', 'false');
-  },
+	handleRowAction: function (component, event, helper) {
+		var action = event.getParam('action');
+		var row = event.getParam('row');
+		switch (action.name) {
+			case 'Edit':
+				helper.edit(component, row, helper);
+				break;
+			case 'Remove':
+				helper.remove(component, row, helper);
+				break;
+		}
+	},
 
-  closeModal: function (component) {
-    component.set('v.showModal', 'false');
-    component.set('v.isRemove', 'false');
-  }
+	updateColumnSorting: function (component, event, helper) {
+		var fieldName = event.getParam('fieldName');
+		var sortDirection = event.getParam('sortDirection');
+		component.set('v.sortedBy', fieldName);
+		var sortedByColumn = '';
+		if (fieldName === 'firstName') {
+			sortedByColumn = $A.get('$Label.c.FirstName');
+		} else if (fieldName === 'lastName') {
+			sortedByColumn = $A.get('$Label.c.LastName');
+		} else if (fieldName === 'email') {
+			sortedByColumn = $A.get('$Label.c.Email');
+		}
+		component.set('v.sortedByColumn', sortedByColumn);
+		component.set('v.sortedBy', fieldName);
+		component.set('v.sortedDirection', sortDirection);
+		helper.sortData(component, fieldName, sortDirection);
+	}
+
 });
---
+++ pkg/main/default/aura/CLMMappedObjectsHome/CLMMappedObjectsHomeHelper.js
@@ -0,0 +1,81 @@
+({
+	sortData: function (component, fieldName, sortDirection) {
+		var data = component.get('v.mappedObjData');
+		var reverse = sortDirection !== 'asc';
+		data.sort(this.sortBy(fieldName, reverse));
+		component.set('v.mappedObjData', data);
+	},
+
+	sortBy: function (field, reverse, primer) {
+		var key = primer
+			? function (x) {
+				return primer(x[field]);
+			}
+			: function (x) {
+				return x[field];
+			};
+		reverse = !reverse ? 1 : -1;
+		return function (a, b) {
+			return (a = key(a)), (b = key(b)), reverse * ((a > b) - (b > a));
+		};
+	},
+
+	edit: function (component, row, helper) {
+		var id = row.Id;
+		var mappedObjData = component.get('v.mappedObjData');
+		var objDetails;
+		mappedObjData.forEach(function (obj) {
+			if (obj.Id === id) {
+				objDetails = obj;
+			}
+		});
+		helper.fireApplicationEvent(
+			component,
+			{
+				data: { objDetails: objDetails },
+				componentName: 'CLMCardModel',
+				fromComponent: 'CLMMappedObjectsHome',
+				toComponent: 'CLMIntegrationLayout',
+				type: 'edit'
+			},
+			'CLMNavigationEvent'
+		);
+		var cmpEvent = component.getEvent('CLMScopedNotificationEvent');
+		cmpEvent.fire();
+	},
+
+	remove: function (component, row, helper) {
+		var id = row.Id;
+		component.set('v.tempMappingModelDataHolder', {
+			id: id,
+			type: 'remove'
+		});
+		helper.createComponent(
+			component,
+			'c:CLMModelFooterButton',
+			{
+				primaryButtonLabel: $A.get('$Label.c.Remove'),
+				secondaryButtonLabel: $A.get('$Label.c.Cancel'),
+				primaryButtonVariant: 'destructive'
+			},
+			function (newCmp) {
+				component.set('v.strikeModelFooterButtons', newCmp);
+				component.set('v.isRemove', 'true');
+			}
+		);
+		var modelTitle = $A.get('$Label.c.RemoveMapping');
+		var modelBody = $A.get('$Label.c.RemoveModalBody');
+		component.set('v.modelTitleText', modelTitle);
+		component.set('v.modalBodyText', modelBody);
+		component.set('v.modelPrimaryButtonText', 'Remove');
+		component.set('v.modelSecondaryButtonText', 'Cancel');
+		component.set('v.showModal', 'true');
+		var modelComponent = component.find('popupModal');
+		setTimeout(
+			$A.getCallback(function () {
+				modelComponent.show();
+			}),
+			5
+		);
+	}
+});
