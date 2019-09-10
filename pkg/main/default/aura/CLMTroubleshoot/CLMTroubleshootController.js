010ea8604161fdf65a93515fa3d433ba43b41c6c
Ramprasad Bommaganty <Ramprasad.Bommaganty@docusign.com>
2019-09-10 16:36:24 -0700

    Initial commit:5855/56

--- pkg/main/default/aura/CLMTroubleshoot/CLMTroubleshootController.js
+++ pkg/main/default/aura/CLMTroubleshoot/CLMTroubleshootController.js
@@ -1,10 +1,18 @@
 ({
-  navigateToSection: function (component, event, helper) {
-    helper.fireApplicationEvent(component, {
-      fromComponent: 'CLMTroubleshoot',
-      toComponent: 'CLMSetupLayout',
-      type: 'update',
-      tabIndex: '4',
-    }, 'CLMNavigationEvent');
-  }
+	navigateToSection: function (component, event, helper) {
+		var tabIndex, eventHandlerComponent;
+		if (component.get('v.hyperlinkedText') === $A.get('$Label.c.DocumentGeneration')) {
+			tabIndex = 4;
+			eventHandlerComponent = 'CLMSetupLayout';
+		} else {
+			tabIndex = 8;
+			eventHandlerComponent = 'CLMHelpLayout'
+		}
+		helper.fireApplicationEvent(component, {
+			fromComponent: 'CLMTroubleshoot',
+			toComponent: eventHandlerComponent,
+			type: 'update',
+			tabIndex: tabIndex,
+		}, 'CLMNavigationEvent');
+	}
 });
