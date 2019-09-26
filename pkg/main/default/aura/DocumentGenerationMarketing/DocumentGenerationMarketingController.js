e4a9840e2c3715627d7da9045db429d87e2f317c
Vineet.Puranik <vineet.puranik@docusign.com>
2019-09-26 12:17:42 -0700

    refactored the component

--- pkg/main/default/aura/DocumentGenerationMarketing/DocumentGenerationMarketingController.js
+++ pkg/main/default/aura/DocumentGenerationMarketing/DocumentGenerationMarketingController.js
@@ -1,4 +1,8 @@
 ({
+  onInitializeComponent: function (component, event, helper) {
+    helper.onInitializeComponent(component);
+  },
+
   toggleGenFeaturesSection: function (component, event, helper) {
     helper.toggleGenFeaturesSection(component);
   },
--- pkg/main/default/aura/DocumentGenerationMarketing/DocumentGenerationMarketingHelper.js
+++ pkg/main/default/aura/DocumentGenerationMarketing/DocumentGenerationMarketingHelper.js
@@ -1,4 +1,9 @@
 ({
+  onInitializeComponent: function (component) {
+    component.set('v.docGenFeatures', [$A.get('$Label.c.GenerateWordOrPdfFeature'), $A.get('$Label.c.MergeDataFeature'), $A.get('$Label.c.ConditionalContentFeature'), $A.get('$Label.c.DynamicPricingFeature'), $A.get('$Label.c.ESignatureCompatibleFeature')]);
+    component.set('v.negotiateFeatures', [$A.get('$Label.c.ApproversFeature'), $A.get('$Label.c.ExternalReviewFeature'), $A.get('$Label.c.VersionControlFeature'), $A.get('$Label.c.RoutingFeature')]);
+  },
+
   toggleGenFeaturesSection: function (component) {
     component.set('v.docGenFeaturesExpanded', !component.get('v.docGenFeaturesExpanded'));
   },
