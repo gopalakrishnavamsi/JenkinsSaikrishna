ee094c416fa141fea845e01b1060822ca64a5eb7
Vineet Puranik <Vineet.Puranik@docusign.com>
2019-11-06 20:10:42 -0800

    DFS-6340 and DFS-6341 Admin Home Page and Collaboration tab updates. (#446)
    * DFS-6340 and DFS-6341 updates
    
    * updates
    
    * Added custom labels

--- pkg/main/default/aura/NegotiateHelp/NegotiateHelpHelper.js
+++ pkg/main/default/aura/NegotiateHelp/NegotiateHelpHelper.js
@@ -2,7 +2,8 @@
   init: function (component) {
     var helpSteps = [ { stepNumber: 1, stepDetail: $A.get('$Label.c.NegotiateHelpText1')}, 
                       { stepNumber: 2, stepDetail: $A.get('$Label.c.NegotiateHelpText2')}, 
-                      { stepNumber: 3, stepDetail: $A.get('$Label.c.NegotiateHelpText3')}
+                      { stepNumber: 3, stepDetail: $A.get('$Label.c.NegotiateHelpText3')},
+                      { stepNumber: 4, stepDetail: $A.get('$Label.c.NegotiateHelpText4')}
                     ];
     component.set('v.helpSteps', helpSteps);
   }
---
+++ pkg/main/default/aura/SetupLearnAboutProducts/SetupLearnAboutProductsController.js
@@ -0,0 +1,5 @@
+({
+  navigateToHelpTab: function (component) {
+    component.set('v.selectedItem', 'tab_help');
+  }
+});
