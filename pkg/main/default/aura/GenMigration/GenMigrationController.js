995e0b041b183f28df6b29ed4701c2c4550d7e74
Avinash Mullapudi <Avinash.Mullapudi@docusign.com>
2020-04-13 21:47:11 -0400

    DFS-7189 (Fix for Gen to DAL migration Batch job error) (#831)
    * Update GenMigrationController.js
    
    * apply code reformatting for GenMigrationController.js
    
    Co-authored-by: Rafaeli Arroyo <Rafaeli.Arroyo@docusign.com>

--- pkg/main/default/aura/GenMigration/GenMigrationController.js
+++ pkg/main/default/aura/GenMigration/GenMigrationController.js
@@ -20,33 +20,49 @@
   migrateUsersConfiguration: function (component, event, helper) {
     // For hiding/showing UserButton
     component.set('v.isUsersMigrated', true);
-    helper.callServer(component, 'c.initiateUserMigration', {},
-      function (response) {
-        var status = response.getState();
-        if (status === 'SUCCESS') {
-          component.set('v.migrationUserStatusMessage', $A.get('$Label.c.OnLoadMigrationText'));
-        } else {
-          component.set('v.isUsersMigrated', false);
-          component.set('v.migrationUserStatusMessage', stringUtils.format($A.get('$Label.c.MigrateTemplateErrorText'),
-            component.get('v.getResult').onLoadUserCount + ' user(s) ', stringUtils.getErrorMessage(response)));
-        }
-      });
+    // adding time interval to avoid duplicate setupOwnerId issue in custom setting,
+    // when user clicks User & Template without any time gap
+    var interval = 0;
+    if (component.get('v.scope') === 'All' && component.get('v.isTemplatesMigrated')) {
+      interval = 500;
+    }
+    setTimeout($A.getCallback(function () {
+      helper.callServer(component, 'c.initiateUserMigration', {},
+        function (response) {
+          var status = response.getState();
+          if (status === 'SUCCESS') {
+            component.set('v.migrationUserStatusMessage', $A.get('$Label.c.OnLoadMigrationText'));
+          } else {
+            component.set('v.isUsersMigrated', false);
+            component.set('v.migrationUserStatusMessage', stringUtils.format($A.get('$Label.c.MigrateTemplateErrorText'),
+              component.get('v.getResult').onLoadUserCount + ' user(s) ', stringUtils.getErrorMessage(response)));
+          }
+        });
+    }), interval);
 
   },
 
   migrateTemplateConfiguration: function (component, event, helper) {
     component.set('v.isTemplatesMigrated', true); // For hiding/showing TemplateButton
-    helper.callServer(component, 'c.initiateTemplateMigration', {},
-      function (response) {
-        var status = response.getState();
-        if (status === 'SUCCESS') {
-          component.set('v.migrationTemplateStatusMessage', $A.get('$Label.c.OnLoadMigrationText'));
-        } else {
-          component.set('v.isTemplatesMigrated', false);
-          component.set('v.migrationTemplateStatusMessage', stringUtils.format($A.get('$Label.c.MigrateTemplateErrorText'),
-            component.get('v.getResult').onLoadTemplateCount + ' template(s)', stringUtils.getErrorMessage(response)));
-        }
-      });
+    // adding time interval to avoid duplicate setupOwnerId issue in custom setting,
+    // when user clicks User & Template button without any time gap
+    var interval = 0;
+    if (component.get('v.scope') === 'All' && component.get('v.isUsersMigrated')) {
+      interval = 500;
+    }
+    setTimeout($A.getCallback(function () {
+      helper.callServer(component, 'c.initiateTemplateMigration', {},
+        function (response) {
+          var status = response.getState();
+          if (status === 'SUCCESS') {
+            component.set('v.migrationTemplateStatusMessage', $A.get('$Label.c.OnLoadMigrationText'));
+          } else {
+            component.set('v.isTemplatesMigrated', false);
+            component.set('v.migrationTemplateStatusMessage', stringUtils.format($A.get('$Label.c.MigrateTemplateErrorText'),
+              component.get('v.getResult').onLoadTemplateCount + ' template(s)', stringUtils.getErrorMessage(response)));
+          }
+        });
+    }), interval);
 
   }
 });
