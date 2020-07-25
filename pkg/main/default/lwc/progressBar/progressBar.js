04a7a0ceb462c6121d6d00919f3c8801fd3990f5
Rafaeli Arroyo <Rafaeli.Arroyo@docusign.com>
2020-07-24 18:34:14 -0700

    update progress bar visuals (#1122)

--- pkg/main/default/lwc/progressBar/progressBar.js
+++ pkg/main/default/lwc/progressBar/progressBar.js
@@ -7,18 +7,57 @@ import {LightningElement, api} from 'lwc';
 
 export default class ProgressBar extends LightningElement {
   @api disableSteps = false;
-  @api currentStep;
-  @api steps;
+  progressSteps = [];
+
+  @api
+  get currentStep() {
+    return this._currentStep;
+  }
+
+  set currentStep(step) {
+    this._currentStep = step;
+    this.progressSteps = this.updateStepVisuals(this.progressSteps);
+  }
+
+  @api
+  get steps() {
+    return this.progressSteps;
+  }
+
+  set steps(val) {
+    this.progressSteps = this.updateStepVisuals(val);
+  }
 
   get progressIndicatorStyle() {
     return this.disableSteps ? 'ds-disabled-path' : '';
   }
 
   handleStep(event) {
-    if (this.disableSteps) return;
+    const toStep = event.currentTarget.dataset.step;
+    if (toStep === this._currentStep || this.disableSteps) return;
+    this.progressSteps = this.updateStepVisuals(this.progressSteps, toStep);
     const progressStepEvent = new CustomEvent('clickprogressstep', {
-      detail: {data: event.target.value}
+      detail: {data: toStep}
     });
     this.dispatchEvent(progressStepEvent);
+  }
+
+  updateStepVisuals(steps, toStep = this._currentStep) {
+    const numericalToStep = parseInt(toStep, 10);
+    return steps.map((s) => {
+      let style = '';
+      const stepNumber = parseInt(s.value, 10);
+      if (stepNumber === numericalToStep) {
+        style = 'slds-path__item slds-is-current slds-is-active';
+      } else if (stepNumber < numericalToStep) {
+        style = 'slds-path__item slds-is-complete';
+      } else {
+        style = 'slds-path__item slds-is-incomplete';
+      }
+      return {
+        ... s,
+        style
+      };
+    });
   }
 }
