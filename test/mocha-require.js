3194c8fbd533683099cc81c216a4bdcae5ff35da
Swarna Gopalan <Swarna.Gopalan@docusign.com>
2020-08-17 07:55:58 -0700

    DFS-8211: Set JEST as default for testing (#1206)

--- test/mocha-require.js
+++
@@ -1,6 +0,0 @@
-//To Create globals for tests -- Define assert/chai/ etc as we go
-//require('./globals'); - after creating globals.js
-
-var moduleAlias = require('module-Alias');
-
-moduleAlias.addPath(__dirname + '/../pkg/main/default/staticresources');
